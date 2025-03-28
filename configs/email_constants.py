3dfc6ab77ac6a3b4051e43d859b531b6d7b6e6e0
ilyab <i-baykov@internet.ru>
2025-03-28 18:05:37 +0300

    Добавлены константы для отправки писем

---
+++ configs/email_constants.py
@@ -0,0 +1,11 @@
+# Максимальный размер файла
+MAX_FILE_SIZE = 20 * 1024 * 1024  # 20 MB in bytes
+
+# Порт
+SMTP_PORT = 587
+
+# Почтовый сервис
+SMTP_SERVER = 'mail.center.rt.ru'
+
+# Отправитель по умолчанию
+MAIL_FROM = "RPA_PROD_Robot@rt.ru"
