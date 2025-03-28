4329407c1c224a3148a634d5060bd67eb3ed22fd
ilyab <i-baykov@internet.ru>
2025-03-28 16:57:18 +0300

    Добавил тесты для TestEmailRecipients

--- src/email_recipients/tests/conftest.py
+++ src/email_recipients/tests/conftest.py
@@ -1,11 +1,20 @@
 # src/email_recipients/tests/conftest.py
+from unittest.mock import patch
 
 import pytest
 
 from src.email_recipients.config_validator import EmailsFileValidator
+from src.email_recipients.email_recipients import EmailRecipients
 
 
 @pytest.fixture
 def email_validator():
     """Фикстура для создания экземпляра EmailsFileValidator."""
     return EmailsFileValidator("dummy_path.xlsx")
+
+
+@pytest.fixture
+def email_recipients():
+    """Фикстура для создания экземпляра EmailRecipients без вызова валидации."""
+    with patch.object(EmailsFileValidator, 'validate', return_value=None):
+        yield EmailRecipients("dummy_path.xlsx")
---
+++ src/email_recipients/tests/test_email_recipients.py
@@ -0,0 +1,27 @@
+from unittest.mock import patch
+from pytest import mark
+
+import pandas as pd
+
+
+class TestEmailRecipients:
+
+    @patch('pandas.read_excel')
+    @mark.parametrize(
+        ["file_contents", "result"],
+        [
+            ({'Email': ['ilya.baykov@rt.ru']}, ['ilya.baykov@rt.ru']),
+            (
+                    {'Email': ['ilya.baykov@rt.ru', 'astemir.dyshekov@rt.ru']},
+                    ['ilya.baykov@rt.ru', 'astemir.dyshekov@rt.ru'])
+        ]
+    )
+    def test_get_recipients_list(self, mock_read_excel, email_recipients, file_contents, result):
+        # Настройка замоканного метода read_excel
+        mock_read_excel.return_value = pd.DataFrame(file_contents)
+
+        # Вызов метода, который будет использовать замоканный read_excel
+        recipients_list = email_recipients.get_recipients_list()
+
+        # Проверка результата
+        assert recipients_list == result
